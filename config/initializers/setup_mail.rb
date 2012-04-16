c5e6c6abc34028117a35c74cafafdf167aeb6815
ugik <georgek@gmail.com>
2012-04-16 18:24:03 -0400

    Mail handler

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,21 @@
 ActionMailer::Base.smtp_settings = {
-	:address		=> "smtp.gmail.com",
-	:port		=> 587,
-	:domain		=> "gmail.com",
-	:user_name	=> "ugikma",
+	:address		=> "smtpout.secureserver.net",
+	:port		=> 80,
+	:domain		=> "text7.com",
+	:user_name	=> "u",
 	:password	=> "cassimere",
 	:authentication => "plain",
 	:enable_starttls_auto => true
 }
+
+
+#ActionMailer::Base.smtp_settings = {
+#	:address		=> "smtp.gmail.com",
+#	:port		=> 587,
+#	:domain		=> "gmail.com",
+#	:user_name	=> "ugikma",
+#	:password	=> "cassimere",
+#	:authentication => "plain",
+#	:enable_starttls_auto => true
+#}
 
