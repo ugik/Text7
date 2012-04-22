efecfd9637a69eb3f6a5d3dedc0c023e3ce32c7e
ugik <georgek@gmail.com>
2012-04-22 12:02:46 -0400

    Update SendGrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,28 @@
 ActionMailer::Base.delivery_method = :smtp
+
+# SendGrid
 ActionMailer::Base.smtp_settings = {
-	:address		=> "smtpout.secureserver.net",
-	:port		=> 80,
+	:address		=> "smtp.sendgrid.net",
+	:port		=> 587,
 	:domain		=> "text7.com",
-	:user_name	=> "u@text7.com",
+	:user_name	=> "app4057330@heroku.com",
 	:password	=> "cassimere",
 	:authentication => "plain",
 	:enable_starttls_auto => true
 }
 
+# GoDaddy
+#ActionMailer::Base.smtp_settings = {
+#	:address		=> "smtpout.secureserver.net",
+#	:port		=> 80,
+#	:domain		=> "text7.com",
+#	:user_name	=> "u@text7.com",
+#	:password	=> "cassimere",
+#	:authentication => "plain",
+#	:enable_starttls_auto => true
+#}
 
+# Gmail
 #ActionMailer::Base.smtp_settings = {
 #	:address		=> "smtp.gmail.com",
 #	:port		=> 587,
