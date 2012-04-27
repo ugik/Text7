3e89dd88d9e1ee7bd975644309d2c4007010c6ce
ugik <georgek@gmail.com>
2012-04-27 01:39:00 -0400

    Update count

--- app/mailers/text_mailer.rb
+++ app/mailers/text_mailer.rb
@@ -296,12 +296,13 @@ class TextMailer < ActionMailer::Base
 				if !default_group.nil?
 					group = Group.find_by_id(default_group)
 					@usergroup = Usergroup.find_all_by_group_id(group.id)
+					count = 0
 					@usergroup.each do |ug|
 						if !ug.user.nil? and ug.user.cell!=email	 # don't send msg to sender
 							sender(ug.user.cell, subject, to_address, body)
+							count+=1
 						end
 					end
-					count = @usergroup.count-1
 					sender(email, "sent #{count} msgs to group:#{group.name}", to_address) # echo back number of msgs sent
 				end
 			end
